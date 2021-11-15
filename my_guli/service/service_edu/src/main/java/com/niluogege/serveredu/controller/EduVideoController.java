883e2181588b403aa0a551beaf98b1667b876dcd
luoChen <hank.luo@xianghuanji.com>
2021-11-15 17:39:56 +0800

    controller service mapper 整套调通

--- my_guli/service/service_edu/src/main/java/com/niluogege/serveredu/controller/EduVideoController.java
+++ my_guli/service/service_edu/src/main/java/com/niluogege/serveredu/controller/EduVideoController.java
@@ -1,8 +1,14 @@
 package com.niluogege.serveredu.controller;
 
 
+import com.niluogege.serveredu.entity.EduVideo;
+import com.niluogege.serveredu.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,5 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/serveredu/edu-video")
 public class EduVideoController {
+
+    @Autowired
+    private EduVideoService eduVideoService;
+
+    @GetMapping("/list")
+    public List<EduVideo> getList(){
+        return eduVideoService.list(null);
+    }
 
 }
