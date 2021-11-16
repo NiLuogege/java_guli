f81ac59436e6b2fd61e111e54cc08c7d4eaec19d
luoChen <hank.luo@xianghuanji.com>
2021-11-16 10:33:04 +0800

    讲师

--- my_guli/service/service_edu/src/main/java/com/niluogege/serveredu/controller/EduTeacherController.java
+++ my_guli/service/service_edu/src/main/java/com/niluogege/serveredu/controller/EduTeacherController.java
@@ -1,8 +1,16 @@
 package com.niluogege.serveredu.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.niluogege.serveredu.entity.EduTeacher;
+import com.niluogege.serveredu.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.ibatis.annotations.Delete;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,8 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @author niluogege
  * @since 2021-11-15
  */
+@Api(tags = "EduTeacherController")
 @RestController
 @RequestMapping("/serveredu/edu-teacher")
 public class EduTeacherController {
+
+    @Autowired
+    private EduTeacherService teacherService;
+
+    @ApiOperation("获取所有讲师")
+    @GetMapping("/")
+    public List<EduTeacher> list() {
+        return teacherService.list(null);
+    }
+
+
+    @ApiOperation("根据ID删除讲师")
+    @DeleteMapping("/{id}")
+    public boolean removeById(
+            @ApiParam(name = "id", value = "讲师id", required = true) @PathVariable String id
+    ) {
+        return teacherService.removeById(id);
+    }
 
 }
