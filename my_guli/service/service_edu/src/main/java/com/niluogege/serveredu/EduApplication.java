671d565cae286786d1630c400b6c9530eec32e07
luoChen <hank.luo@xianghuanji.com>
2021-11-16 10:19:00 +0800

    配置 swagger

--- my_guli/service/service_edu/src/main/java/com/niluogege/serveredu/EduApplication.java
+++ my_guli/service/service_edu/src/main/java/com/niluogege/serveredu/EduApplication.java
@@ -3,8 +3,11 @@ package com.niluogege.serveredu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.niluogege"})
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
--- my_guli/service/service_edu/src/main/java/com/niluogege/serveredu/controller/HelloController.java
+++ my_guli/service/service_edu/src/main/java/com/niluogege/serveredu/controller/HelloController.java
@@ -1,11 +1,15 @@
 package com.niluogege.serveredu.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "HelloController")
 @RestController
 public class HelloController {
 
+    @ApiOperation("hello")
     @GetMapping("/hello")
     public String hehe() {
         return "fffa";
